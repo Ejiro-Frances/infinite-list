56b388311b7523e26e86fc066423d8b8124f5138
Ejiro-Frances <francesejiro@yahoo.com>
2025-11-18 19:13:46 +0100

    chore: removed unnecessary comment

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,6 @@ import PiList from "@/components/pi-list";
 const page = () => {
   return (
     <div className="max-w-3xs mx-auto py-10 border border-xl">
-      {/* <p className="small">
-        Digits of Pi (after the decimal point). Virtualized list + chunked
-        server generation.
-      </p> */}
       <div style={{ marginTop: 12 }}>
         <PiList chunkSize={1000} />
       </div>
